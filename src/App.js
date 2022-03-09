e5b6a14c24ee2f51ee11645c5b3b93ef6b636259
Mateus Benites Dias <imattdias@gmail.com>
2022-03-09 18:56:33 -0300

    Projeto Limpo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import imagem from './assets/homem-feliz-dinheiro.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Projeto Limpo!</h1>
+      <img src={imagem} alt="homem feliz" />
+    </>
   );
 }
 
